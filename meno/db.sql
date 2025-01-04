7f8b5de631f30e31043ba352bdc17f42466e0a03
Gompangii <gompangii@gmail.com>
2025-01-04 21:09:02 +0900

    5.4.2 플라웨이를 이용한 데이터베이스 스키마 초기화

--- meno/db.sql
+++ meno/db.sql
@@ -3,15 +3,21 @@ DROP DATABASE IF EXISTS polardb_catalog;
 CREATE DATABASE polardb_catalog;
 USE polardb_catalog;
 
-
+# schema.sql 파일 내용 플라웨이 적용을 위해 삭제 함
 DROP TABLE IF EXISTS book;
 CREATE TABLE book (
-	id 						INT UNSIGNED NOT NULL PRIMARY KEY,
-	author			VARCHAR(255) NOT NULL,
-	isbn					VARCHAR(255) NOT NULL UNIQUE,
-	price				DOUBLE NOT NULL,
-	title						VARCHAR(255) NOT NULL,
-	`version`	INT NOT NULL
+	id                  INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	author              VARCHAR(255) NOT NULL,
+	isbn                VARCHAR(255) NOT NULL UNIQUE,
+	price               DOUBLE NOT NULL,
+	title               VARCHAR(255) NOT NULL,
+	created_date        TIMESTAMP NOT NULL,
+    last_modified_date  TIMESTAMP NOT NULL,
+	`version`           INT NOT NULL
 );
 
-DESC book;
+# application.yml 파일에서 플라웨이 적용을 위해 삭제
+  sql:
+    init:
+      mode: always
+
