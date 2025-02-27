e922ce6e5ea2e0915bdc1b4eabf865d0c152cc4a
Gompangii <gompangii@gmail.com>
2025-02-27 14:52:53 +0900

    데모데이터 추가

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -23,7 +23,8 @@ public class BookDataLoader {
     bookRepository.deleteAll();
     var book1 = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90, "Polarsophia");
     var book2 = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90, "Polarsophia");
+    var book3 = Book.of("1234567893", "제목", "Iorek Polarson", 12.90, "Polarsophia");
     //bookRepository.save(book1);
-    bookRepository.saveAll(List.of(book1, book2));
+    bookRepository.saveAll(List.of(book1, book2, book3));
   }
 }
