560cae957584e5f3c829ee7c85d623674387efbe
Gompangii <gompangii@gmail.com>
2025-02-27 10:41:39 +0900

    Dokerfile 작성

--- memo/db.sql
+++ memo/db.sql
@@ -21,3 +21,20 @@ CREATE TABLE book (
     init:
       mode: always
 
+
+# 보안을 위한 스키마 변경
+DROP TABLE IF EXISTS book;
+CREATE TABLE book (
+	id                  INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	author              VARCHAR(255) NOT NULL,
+	isbn                VARCHAR(255) NOT NULL UNIQUE,
+	price               DOUBLE NOT NULL,
+	title               VARCHAR(255) NOT NULL,
+	created_date        TIMESTAMP NOT NULL,
+    last_modified_date  TIMESTAMP NOT NULL,
+    created_by 			VARCHAR(255),
+    last_modified_by    VARCHAR(255),
+	`version`           INT NOT NULL
+);
+
+
